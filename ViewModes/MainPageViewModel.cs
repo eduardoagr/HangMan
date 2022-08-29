cbaba7cb341661e3a1b5d6d741699b1362fcfd16
Eduado Gomez <egomezr@outlook.com>
2022-08-29 04:49:25 +0200

    Update the name

--- ViewModes/MainPageViewModel.cs
+++ ViewModes/MainPageViewModel.cs
@@ -11,7 +11,7 @@ public partial class MainPageViewModel : ObservableObject
     readonly List<char> charList = new();
 
     [ObservableProperty]
-    private string sporlight;
+    private string _spotlight;
 
     public MainPageViewModel(WordService wordService)
     {
@@ -28,7 +28,7 @@ public partial class MainPageViewModel : ObservableObject
     private void CalculateWord(string ans, List<char> guessed)
     {
         var temp = ans.Select(x => guessed.IndexOf(x) >= 0 ? x : '_').ToArray();
-        sporlight = string.Join(' ', temp);
+        Spotlight = string.Join(' ', temp);
     }
 
 
